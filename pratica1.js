7735976185ead479cd80c69bb086380c69274322
Leandro Siqueira <leandrosiqueira00798@gmail.com>
2026-06-01 22:00:01 -0300

    Atividade de revisão

---
+++ pratica1.js
@@ -0,0 +1,34 @@
+const botao = document.getElementById("btnMensagem");
+
+if (!botao) {
+
+} else {
+
+    botao.addEventListener("click", function () {
+        alert("olá! O pão de queijo quentinho acaba de sair do forno");
+        console.log("Botão de boas-vindas clicando.");
+
+
+
+    });
+    
+
+};
+
+
+const botao1 = document.getElementById("btnPromoção");
+
+if (!botao1) {
+
+} else {
+
+    botao1.addEventListener("click", function () {
+        alert("Croissant de chocolate com 20% de desconto hoje!");
+        console.log("Botão de boas-vindas clicando.");
+
+
+
+    });
+    
+
+};
