ed86aa81a1246aa632fb66ec4fbc146e0dc131e2
Leandro <leandrosiqueira00798@gmail.com>
2026-06-03 20:01:06 -0300

    Atividade Estrutura Condicional

---
+++ idade.js
@@ -0,0 +1,49 @@
+let idade = 25;
+const pais = "brasil";
+
+
+// Mudando o valor 
+idade = 20; 
+
+console.log(idade); // Exibe 20 
+console.log(pais); // Exibe Brasil 
+
+// Exenplo de uso no JavaScript
+
+let preco = 50;
+let quantidade = 3;
+let frete = 15; 
+
+// Cálculo matematico 
+let total = (preco * quantidade) + frete;
+
+console.log(total); // Resultado:  165 
+
+ // O Dobro 
+
+let idade = 50;
+const pais = "brasil";
+
+idade = 25;
+
+// A Troca 
+
+let a = 10;
+console.log("a:" + a );
+let b = 20
+console.log("b: "+b);
+let c = a;
+a = b;
+b = a;
+console.log("A trocs de a e b é ")
+console.log(" ")
+console.log(" ")
+console.log(" ")
+
+// Variaveis 
+
+
+
+
+
+
