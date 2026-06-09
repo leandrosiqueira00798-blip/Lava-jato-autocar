d26b777e401f35d3cdc228e6c97d4f8c4c9a9a30
Leandro <leandrosiqueira00798@gmail.com>
2026-06-09 11:52:13 -0300

    Atividade de array e matriz

---
+++ array.js
@@ -0,0 +1,52 @@
+let frutas = [
+"Maçãs",
+"banana",
+"leite",
+"Ovos",
+"Café",
+
+
+
+];
+
+/*console.log(frutas);
+
+console.log(frutas[0]);
+console.log(frutas[1]);
+
+let total = frutas.length;
+console.log(total);
+console.log(frutas[frutas.length - 1]);
+
+frutas.push("Abacaxi");
+total = frutas.length;
+console.log(frutas);
+console.log(total);
+console.log(frutas[frutas.length - 1]);
+
+
+
+ltima = frutas.pop();
+console.log(frutas);
+console.log(ultima );
+total = frutas.length;
+console.log(total);
+console.log(frutas[frutas.length-1]);
+
+let frutas = ["Maçã","Banana","Uva"];*/
+
+for (let i = 0; i< frutas.length; i++) {
+    console.log(frutas[i]);
+}
+let produtos = [
+["Pão", 5.00],
+["Leite", 4.50],
+["café", 15.00],
+
+];
+
+let Pão = produtos[0][0];
+let preco = produtos[0][1];
+console.log(produtos);
+console.log(produtos[1][0]);
+
