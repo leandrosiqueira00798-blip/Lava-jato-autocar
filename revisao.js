5562f6226cc8a6bfeff0e65e7f3ba3467bd32b6a
Leandro <leandrosiqueira00798@gmail.com>
2026-06-10 21:53:47 -0300

    Atividade de Revisão

---
+++ revisao.js
@@ -0,0 +1,38 @@
+/*const nota1 = 7.5;
+const nota2 = 8.0;
+const nota3 = 6.5;
+
+const soma = nota1 + nota2 + nota3;
+
+const quantidade = 3; 
+const media = soma / quantidade;
+
+console.log("Media",media);*/
+
+/*for(let i=0;i>5;i++){
+    console.log("olá");
+}*/
+
+/*--- loop infinito---*/
+
+/*let i=0;
+while(i<5){
+     console.log("olá");
+     i++*
+}*/
+
+//Array//
+
+const frutas = ["pera", "abacate", "morango"];
+
+console.log(pera);
+console.log(abacate);
+console.log(morango);
+
+const primeiro = frutas.shift()
+
+ console.log(frutas.length);
+
+console.log(frutas);
+
+
